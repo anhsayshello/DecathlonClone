d427b4b799673b025dbb41f81a9301f99db10288
anhsayshello <anhle0347@gmail.com>
2025-05-21 11:32:21 +0700

    remove skeleton loader

--- src/layouts/MainLayout/MainLayout.tsx
+++ src/layouts/MainLayout/MainLayout.tsx
@@ -1,20 +1,11 @@
-import { useIsFetching } from '@tanstack/react-query'
 import { Outlet } from 'react-router'
 import Footer from 'src/components/Footer'
 import Header from 'src/components/Header'
 
 export default function MainLayout() {
-  const totalFetching = useIsFetching()
-  const fetchingPurchases = useIsFetching()
-  const fetchingExceptPurchases = totalFetching - fetchingPurchases
   return (
     <>
       <Header />
-      {fetchingExceptPurchases > 0 && (
-        <div className='pt-19 lg:pt-22 px-5 lg:px-10'>
-          <div className='skeleton h-screen w-full rounded-sm'></div>
-        </div>
-      )}
       <Outlet />
       <Footer />
     </>
