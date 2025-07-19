12ea9c5be47c5993d99b511ed8d2e1dc6264137a
anhsayshello <anhle0347@gmail.com>
2025-07-19 14:16:45 +0700

    update gemini api key

--- src/apis/chatbot.api.tsx
+++ src/apis/chatbot.api.tsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 import ChatbotResponse from 'src/types/chatbot.type'
 
 const chatbotApi = (contents: { role: string; parts: { text: string }[] }[]) =>
-  axios.post<ChatbotResponse>(import.meta.env.GEMINI_API, JSON.stringify({ contents }), {
+  axios.post<ChatbotResponse>(import.meta.env.GEMINI_API_KEY, JSON.stringify({ contents }), {
     headers: { 'Content-Type': 'application/json' }
   })
 
